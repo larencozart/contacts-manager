5dccfa2dcecad009d850a86cf02821b7aac0790d
larebear007 <larencozart@gmail.com>
2024-04-22 16:58:45 -0500

    Add input validation for empty entries

--- contacts.js
+++ contacts.js
@@ -45,6 +45,7 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false}));
 app.use(morgan("common"));
 
 app.get("/", (req, res) => {
@@ -56,6 +57,50 @@ app.get("/contacts", (req, res) => {
     contacts: sortContacts(contactData)
   });
 });
+
+app.get("/contacts/new", (req, res) => {
+  res.render("new-contact");
+});
+
+app.post("/contacts/new",
+  (req, res, next) => {
+    res.locals.errorMessages = [];
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.firstName.length === 0) {
+      res.locals.errorMessages.push("First name is required.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.lastName.length === 0) {
+      res.locals.errorMessages.push("Last name is required.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.phoneNumber.length === 0) {
+      res.locals.errorMessages.push("Phone number is required.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (res.locals.errorMessages.length > 0) {
+      res.render("new-contact", {
+        errorMessages: res.locals.errorMessages,
+      });
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    contactData.push({...req.body});
+    res.redirect("/contacts");
+  });
 
 app.listen(3000, "localHost", () => {
   console.log("Listening to port 3000");
